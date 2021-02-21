474e593970b01e5eddb417a3259ec56eedb2e6c4
cesar <cesar@DESKTOP-07BF4QK>
2021-02-21 00:11:26 -0600

    Actividad 2.5 Clases y Objetos

---
+++  Activ25-ClasesObjetos/src/ConjuntoEnteros/AplicacionConjuntoEnteros.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConjuntoEnteros;
+
+/**
+ *
+ * @author cesar
+ */
+public class AplicacionConjuntoEnteros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
