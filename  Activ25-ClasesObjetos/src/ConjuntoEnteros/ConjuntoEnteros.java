11cef086252567a9a884ed19098a839fad646477
cesar <cesar@DESKTOP-07BF4QK>
2021-03-05 08:22:34 -0600

    conjunto enteros

---  Activ25-ClasesObjetos/src/ConjuntoEnteros/ConjuntoEnteros.java
+++  Activ25-ClasesObjetos/src/ConjuntoEnteros/ConjuntoEnteros.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ConjuntoEnteros;
 
-/**
- *
- * @author cesar
- */
+
 public class ConjuntoEnteros {
+    private boolean valores[];
+
+    public ConjuntoEnteros() {
+        valores = new boolean[101];
+        for (int i = 0; i <= valores.length; i++) {
+           this.valores[i] = false;
+        }
+    }
     
+    
+
 }
