df137f6014832ee11a7e1610a4be9b9e53f1d1c3
cesar <cesar@DESKTOP-07BF4QK>
2021-04-29 23:04:52 -0500

    Activ 6.2 Secuencias De Caracteres

--- Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/AplicacionGestionArchivosCaracteres.java
+++ Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/AplicacionGestionArchivosCaracteres.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SecuenciasDeCaracteres;
 
-/**
- *
- * @author cesar
- */
+import java.io.IOException;
+
+
 public class AplicacionGestionArchivosCaracteres {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) throws IOException {
+        GestiionArchivoCaracteres archivo = new GestiionArchivoCaracteres();
+        archivo.escribirCaracteres();
+        archivo.leerCaracteres();
     }
     
 }
--- Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/GestiionArchivoCaracteres.java
+++ Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/GestiionArchivoCaracteres.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SecuenciasDeCaracteres;
 
-/**
- *
- * @author cesar
- */
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GestiionArchivoCaracteres {
-    
+
+    public GestiionArchivoCaracteres() {
+
+    }
+
+    public void escribirCaracteres() {
+        try {
+            FileWriter salida = new FileWriter("DatosPersonales");
+            salida.write("Cesar Enoc Rodriguez Villanueva \n15 de Mayo del 2001 \nCiudad Victoria, Tamaulipas");
+            salida.close();
+        } catch (IOException ex) {
+            Logger.getLogger(GestiionArchivoCaracteres.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("el archivo no se pudo escribir");
+        }
+    }
+
+    public void leerCaracteres() {
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader("DatosPersonales"));
+            String cadena;
+            while ((cadena = entrada.readLine()) != null) {
+                System.out.println(""+ cadena);
+            }
+            entrada.close();
+        } catch (IOException e) {
+            Logger.getLogger(GestiionArchivoCaracteres.class.getName()).log(Level.SEVERE, null, e);
+            System.out.println("El archivo no esta disponible");
+        }
+
+    }
 }
