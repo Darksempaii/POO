654c9121e1694fb5fb5d7b7b6a0414784f26d578
cesar <cesar@DESKTOP-07BF4QK>
2021-05-03 08:33:07 -0500

    Activ 6.2 Secuencias De Caracteres

--- Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/AplicacionGestionArchivosCaracteres.java
+++ Activ62-SecuenciasDeCaracteres/src/SecuenciasDeCaracteres/AplicacionGestionArchivosCaracteres.java
@@ -5,10 +5,10 @@ public class AplicacionGestionArchivosCaracteres {
     public static void main(String[] args) {
         GestiionArchivoCaracteres archivo = new GestiionArchivoCaracteres();
         archivo.escribirCaracteres("DatosPersonales.txt");
-        archivo.leerCaracteres("DatosPersonales.txt");
+        System.out.println(archivo.leerCaracteres("DatosPersonales.txt"));
         System.out.println("");
         archivo.escribirCaracteres("NuevosDatosPersonales.txt");
-        archivo.leerCaracteres("NuevosDatosPersonales.txt");
+        System.out.println(archivo.leerCaracteres("NuevosDatosPersonales.txt"));
     }
 
 }
