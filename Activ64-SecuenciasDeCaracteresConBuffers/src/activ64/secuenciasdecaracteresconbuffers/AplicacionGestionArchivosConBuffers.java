93c1b81a2b756e32bed9d04f343a27049596bf1e
cesar <cesar@DESKTOP-07BF4QK>
2021-05-06 10:10:26 -0500

    Activ 6.4 Secuencias De Caracteres con bufferes

--- Activ64-SecuenciasDeCaracteresConBuffers/src/activ64/secuenciasdecaracteresconbuffers/AplicacionGestionArchivosConBuffers.java
+++ Activ64-SecuenciasDeCaracteresConBuffers/src/activ64/secuenciasdecaracteresconbuffers/AplicacionGestionArchivosConBuffers.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package activ64.secuenciasdecaracteresconbuffers;
 
-/**
- *
- * @author cesar
- */
+
 public class AplicacionGestionArchivosConBuffers {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        GestionArchivosConBuffers Datos = new GestionArchivosConBuffers();
+        Datos.escribirCaracteres("DatosPersonales.txt", "NuevosDatosPersonales.txt");
+ 
     }
     
 }
--- Activ64-SecuenciasDeCaracteresConBuffers/src/activ64/secuenciasdecaracteresconbuffers/GestionArchivosConBuffers.java
+++ Activ64-SecuenciasDeCaracteresConBuffers/src/activ64/secuenciasdecaracteresconbuffers/GestionArchivosConBuffers.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package activ64.secuenciasdecaracteresconbuffers;
 
-/**
- *
- * @author cesar
- */
+import java.io.*;
+
 public class GestionArchivosConBuffers {
-    
+
+    public GestionArchivosConBuffers() {
+    }
+
+    public void escribirCaracteres(String archivo1, String archivo2) {
+        String leer = "";
+        try {
+            BufferedWriter salida = new BufferedWriter(new FileWriter(archivo2));
+            BufferedReader entrada = new BufferedReader(new FileReader(archivo1));
+            String cadena;
+            while ((cadena = entrada.readLine()) != null) {
+                leer = leer + cadena + "\n";
+            }
+            System.out.print(leer);
+            salida.write(leer);
+            entrada.close();
+            salida.close();
+        } catch (IOException e) {
+            System.out.println("el archivo no se pudo escribir: " + e);
+        }
+    }
 }
